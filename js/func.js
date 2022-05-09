170582c158888c11bf91fbc29fa695f57c6750b8
shin0510 <75784915+shin0510@users.noreply.github.com>
2022-05-09 09:27:28 +0900

    Add files via upload

---
+++ js/func.js
@@ -0,0 +1,153 @@
+$(function(){
+   
+  // 「.modal_open」をクリックしたらモーダルと黒い背景を表示する
+  $(function(){
+ 
+    // 黒い背景をbody内に追加
+    $('body').append('<div class="modal_bg"></div>');
+    $('.modal_bg').fadeIn();
+ 
+    // data-targetの内容をIDにしてmodalに代入
+    var modal = '#' + $('.modal_open').attr('data-target');
+ 
+    // モーダルをウィンドウの中央に配置する
+    function modalResize(){
+        var w = $(window).width();
+        var h = $(window).height();
+ 
+        var x = (w - $(modal).outerWidth(true)) / 2;
+      var y = (h - $(modal).outerHeight(true)) / 2;
+ 
+      $(modal).css({ 'left': x + 'px', 'top': y + 'px' });
+      
+    }
+ 
+    // modalResizeを実行
+    modalResize();
+ 
+    // modalをフェードインで表示
+    $(modal).fadeIn();
+ 
+    // .modal_bgか.modal_closeをクリックしたらモーダルと背景をフェードアウトさせる
+    $('.modal_close2').off().click(function(){
+        $('.modal_box').fadeOut();
+        $('.modal_bg').fadeOut('slow',function(){
+            $('.modal_bg').remove();
+        });
+    });
+ 
+    // ウィンドウがリサイズされたらモーダルの位置を再計算する
+    $(window).on('resize', function(){
+        modalResize();
+    });
+ 
+    // .modal_switchを押すとモーダルを切り替える
+    $('.modal_switch').click(function(){
+ 
+      // 押された.modal_switchの親要素の.modal_boxをフェードアウトさせる
+      $(this).parents('.modal_box').fadeOut();
+ 
+      // 押された.modal_switchのdata-targetの内容をIDにしてmodalに代入
+      var modal = '#' + $(this).attr('data-target');
+ 
+      // モーダルをウィンドウの中央に配置する
+      function modalResize(){
+          var w = $(window).width();
+          var h = $(window).height();
+ 
+          var x = (w - $(modal).outerWidth(true)) / 2;
+          var y = (h - $(modal).outerHeight(true)) / 2;
+ 
+          $(modal).css({'left': x + 'px','top': y + 'px'});
+      }
+ 
+      // modalResizeを実行
+      modalResize();
+ 
+      $(modal).fadeIn();
+ 
+      // ウィンドウがリサイズされたらモーダルの位置を再計算する
+      $(window).on('resize', function(){
+          modalResize();
+      });
+    });
+    $('.modal_switch2').click(function(){
+ 
+      // 押された.modal_switchの親要素の.modal_boxをフェードアウトさせる
+      $(this).parents('.modal_box').fadeOut();
+ 
+      // 押された.modal_switchのdata-targetの内容をIDにしてmodalに代入
+      var modal = '#' + $(this).attr('data-target');
+ 
+      // モーダルをウィンドウの中央に配置する
+      function modalResize(){
+          var w = $(window).width();
+        var h = $(window).height();
+        var hh = h - 10;
+ 
+          var x = (w - $(modal).outerWidth(true)) / 2;
+          var y = (h - $(modal).outerHeight(true)) / 2;
+ 
+          $(modal).css({'left': x + 'px','top': y + 'px',"max-height":`${hh}px`});
+      }
+      modalResize();
+ 
+      $(modal).fadeIn();
+ 
+      // ウィンドウがリサイズされたらモーダルの位置を再計算する
+      $(window).on('resize', function(){
+          modalResize();
+      });
+    });
+    $('.modal_switch3').click(function(){
+ 
+      // 押された.modal_switchの親要素の.modal_boxをフェードアウトさせる
+      $(this).parents('.modal_box').fadeOut();
+ 
+      // 押された.modal_switchのdata-targetの内容をIDにしてmodalに代入
+      var modal = '#' + $(this).attr('data-target');
+ 
+      // モーダルをウィンドウの中央に配置する
+      function modalResize(){
+          var w = $(window).width();
+        var h = $(window).height();
+        var hh = h - 10;
+ 
+          var x = (w - $(modal).outerWidth(true)) / 2;
+          var y = (h - $(modal).outerHeight(true)) / 2;
+ 
+          $(modal).css({'left': x + 'px','top': y + 'px',"max-height":`${hh}px`});
+      }
+ 
+      // modalResizeを実行
+      modalResize();
+ 
+      $(modal).fadeIn();
+ 
+      // ウィンドウがリサイズされたらモーダルの位置を再計算する
+      $(window).on('resize', function(){
+          modalResize();
+      });
+    });
+  });
+  $('[name="check"]:radio').change( function() {
+    if ($('[id=radio1]').prop('checked')) {
+      $('label[for="radio1"]').text("選択中");
+      $('label[for="radio2"]').text("選択する");
+      $('label[for="radio3"]').text("選択する");
+    } else if ($('[id=radio2]').prop('checked')) {
+      $('label[for="radio2"]').text("選択中");
+      $('label[for="radio1"]').text("選択する");
+      $('label[for="radio3"]').text("選択する");
+    } else if ($('[id=radio3]').prop('checked')) {
+      $('label[for="radio3"]').text("選択中");
+      $('label[for="radio2"]').text("選択する");
+      $('label[for="radio1"]').text("選択する");
+    }
+  });
+  $(".more").click(function () {
+    $(".pc-list").toggleClass("open");
+    $(this).text("閉じる");
+    $(this).toggleClass("more-open");
+  });
+});
